9aca8729b09d810a130a674c1ee43ddaf0af2b0b
Samuel Rembisz <sjrembisz07@gmail.com>
2022-09-05 16:08:49 -0400

    Moved to utilizing polling instead of calling block_on

--- src/multipart.rs
+++ src/multipart.rs
@@ -2,18 +2,37 @@
 #![allow(unused_must_use)]
 
 use crate::ReadSeek;
+use futures::{AsyncReadExt, FutureExt};
 use rand;
-use rocket::futures::io::Cursor;
-use rocket::futures::AsyncWriteExt;
-use rocket::tokio::io::ReadBuf;
-use rocket::tokio::runtime::Handle;
 use rocket::{
-    futures::executor::block_on,
-    tokio::io::{AsyncRead, AsyncSeek, AsyncSeekExt},
+    futures::{io::Cursor, AsyncWriteExt},
+    tokio::io::{AsyncRead, AsyncSeek, AsyncSeekExt, ReadBuf},
 };
-use std::io::SeekFrom;
-use std::pin::Pin;
-use std::task::{Context, Poll};
+use std::{
+    io::SeekFrom,
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+macro_rules! try_poll {
+    ($e:expr) => {
+        match $e {
+            Poll::Ready(Ok(t)) => t,
+            Poll::Ready(Err(e)) => return Poll::Ready(Err(e)),
+            Poll::Pending => return Poll::Pending,
+        }
+    };
+}
+
+macro_rules! return_poll {
+    ($e:expr) => {
+        match $e {
+            Poll::Ready(Ok(t)) => return Poll::Ready(Ok(t)),
+            Poll::Ready(Err(e)) => return Poll::Ready(Err(e)),
+            Poll::Pending => return Poll::Pending,
+        }
+    };
+}
 
 pub type Ranges = Vec<(u64, u64)>;
 
@@ -79,11 +98,9 @@ impl<'a> MultipartReader<'a> {
     }
 
     /// write the boundary into the buffer
-    fn write_boundary(&mut self) -> std::io::Result<()> {
-        let handle = Handle::current();
-        handle.enter();
+    fn write_boundary(&mut self, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
         let boundary = format!("\r\n--{}\r\n", self.boundary);
-        block_on(self.buffer.write_all(boundary.as_bytes()))
+        return_poll!(self.buffer.write_all(boundary.as_bytes()).poll_unpin(cx));
     }
 
     /// Write a header to buffer
@@ -91,51 +108,46 @@ impl<'a> MultipartReader<'a> {
         &mut self,
         header: impl AsRef<str>,
         value: impl AsRef<str>,
-    ) -> std::io::Result<()> {
-        let handle = Handle::current();
-        handle.enter();
+        cx: &mut Context<'_>,
+    ) -> Poll<std::io::Result<()>> {
         let header = format!("{}: {}\r\n", header.as_ref(), value.as_ref());
-        block_on(self.buffer.write_all(header.as_bytes()))
+        return_poll!(self.buffer.write_all(header.as_bytes()).poll_unpin(cx));
     }
 
     /// Write CRLF to buffer
-    fn write_boundary_end(&mut self) -> std::io::Result<()> {
-        let handle = Handle::current();
-        handle.enter();
-        block_on(self.buffer.write_all("\r\n".as_bytes()))
+    fn write_boundary_end(&mut self, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+        return_poll!(self.buffer.write_all("\r\n".as_bytes()).poll_unpin(cx));
     }
 
     // Close the multipart form by sending the boundary closer field.
-    fn write_boundary_closer(&mut self) -> std::io::Result<()> {
-        let handle = Handle::current();
-        handle.enter();
-        block_on(
-            self.buffer
-                .write_all(format!("\r\n--{}--\r\n\r\n", &self.boundary).as_bytes()),
-        )
+    fn write_boundary_closer(&mut self, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+        return_poll!(self
+            .buffer
+            .write_all(format!("\r\n--{}--\r\n\r\n", &self.boundary).as_bytes())
+            .poll_unpin(cx));
     }
 
     /// Empty the buffer by truncating the underlying vector
-    fn clear_buffer(&mut self) {
-        let handle = Handle::current();
-        handle.enter();
-        block_on(rocket::futures::AsyncSeekExt::seek(
-            &mut self.buffer,
-            SeekFrom::Start(0),
-        ))
-        .unwrap();
-        self.buffer.get_mut().truncate(0);
+    fn clear_buffer(&mut self, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+        match rocket::futures::AsyncSeekExt::seek(&mut self.buffer, SeekFrom::Start(0))
+            .poll_unpin(cx)
+        {
+            Poll::Ready(Ok(_)) => {
+                self.buffer.get_mut().truncate(0);
+                Poll::Ready(Ok(()))
+            }
+            Poll::Ready(Err(e)) => Poll::Ready(Err(e)),
+            Poll::Pending => Poll::Pending,
+        }
     }
 }
 
 impl<'a> AsyncRead for MultipartReader<'a> {
     fn poll_read(
         mut self: Pin<&mut MultipartReader<'a>>,
-        _cs: &mut Context,
+        cx: &mut Context,
         buf: &mut ReadBuf<'_>,
-    ) -> Poll<Result<(), std::io::Error>> {
-        let handle = Handle::current();
-        handle.enter();
+    ) -> Poll<std::io::Result<()>> {
         // The number of bytes read into the buffer so far.
         let mut c = 0;
 
@@ -146,16 +158,12 @@ impl<'a> AsyncRead for MultipartReader<'a> {
             // If the buffer isn't empty send the content within it
             if self.buffer.position() < bufsize - 1 {
                 // read operations on the buffer cannot fail.
-                c = block_on(rocket::futures::AsyncReadExt::read(
-                    &mut self.buffer,
-                    buf.initialized_mut(),
-                ))
-                .unwrap();
+                c = try_poll!(self.buffer.read(buf.initialized_mut()).poll_unpin(cx));
             }
 
             // Clear the buffer if all of it has already been read
             if self.buffer.position() >= bufsize - 1 {
-                self.clear_buffer();
+                self.clear_buffer(cx);
             }
         }
 
@@ -169,69 +177,69 @@ impl<'a> AsyncRead for MultipartReader<'a> {
         // Returning 0 for all future calls.
         if self.idx >= self.ranges.len() {
             if self.idx == self.ranges.len() {
-                self.write_boundary_closer()?;
-                block_on(rocket::futures::AsyncSeekExt::seek(
+                try_poll!(self.write_boundary_closer(cx));
+                try_poll!(rocket::futures::AsyncSeekExt::seek(
                     &mut self.buffer,
-                    SeekFrom::Start(0),
-                ))
-                .unwrap();
+                    SeekFrom::Start(0)
+                )
+                .poll_unpin(cx));
 
                 // Because this is one greater than the length
                 // The function will return Ok(0) from now on
                 self.idx = self.idx + 1;
-                return match block_on(rocket::tokio::io::AsyncReadExt::read(
+                try_poll!(rocket::tokio::io::AsyncReadExt::read(
                     &mut self,
                     &mut buf.initialized_mut()[c..],
-                )) {
-                    Ok(_) => Poll::Ready(Ok(())),
-                    Err(e) => Poll::Ready(Err(e)),
-                };
+                )
+                .boxed()
+                .poll_unpin(cx));
             }
             return Poll::Ready(Ok(()));
         }
 
         // Write the range data into the remaining space in the buffer
         let (start, end) = self.ranges[self.idx];
-        let current_position = match block_on(self.stream.stream_position()) {
-            Ok(pos) => pos,
-            Err(e) => return Poll::Ready(Err(e)),
-        };
+        let current_position = try_poll!(self.stream.stream_position().boxed().poll_unpin(cx));
 
         // If we have not written a boundary header yet, we are at the beginning of a new stream
         // Write a boundary header and prepare the stream
         if !self.wrote_boundary_header {
-            match block_on(self.stream.seek(SeekFrom::Start(start))) {
-                Ok(_) => (),
-                Err(e) => return Poll::Ready(Err(e)),
-            };
-            self.write_boundary()?;
+            try_poll!(self
+                .stream
+                .seek(SeekFrom::Start(start))
+                .boxed()
+                .poll_unpin(cx));
+
+            try_poll!(self.write_boundary(cx));
+
             let stream_len = self.stream_len;
-            self.write_boundary_header(
+            try_poll!(self.write_boundary_header(
                 "Content-Range",
                 format!("bytes {}-{}/{}", start, end, stream_len).as_str(),
-            )?;
+                cx
+            ));
+
             let content_type = self.content_type.clone();
-            self.write_boundary_header("Content-Type", content_type)?;
-            self.write_boundary_end()?;
+            try_poll!(self.write_boundary_header("Content-Type", content_type, cx));
+            try_poll!(self.write_boundary_end(cx));
 
             self.wrote_boundary_header = true;
 
             // Seek the buffer back to the start to prepare for being read
             // In the next call.
-            block_on(rocket::futures::AsyncSeekExt::seek(
-                &mut self.buffer,
-                SeekFrom::Start(0),
-            ))
-            .unwrap();
+            try_poll!(
+                rocket::futures::AsyncSeekExt::seek(&mut self.buffer, SeekFrom::Start(0),)
+                    .poll_unpin(cx)
+            );
 
             // Read until the boundary_header is done being sent
-            return match block_on(rocket::tokio::io::AsyncReadExt::read(
+            try_poll!(rocket::tokio::io::AsyncReadExt::read(
                 &mut self,
                 &mut buf.initialized_mut()[c..],
-            )) {
-                Ok(_) => Poll::Ready(Ok(())),
-                Err(e) => Poll::Ready(Err(e)),
-            };
+            )
+            .boxed()
+            .poll_unpin(cx));
+            return Poll::Ready(Ok(()));
         }
 
         // Number of bytes remaining until the end
@@ -242,25 +250,24 @@ impl<'a> AsyncRead for MultipartReader<'a> {
         // And increment the range idx by one to move onto the next
         // Set "wrote_boundary_header" to false, allowing the next header to be created
         if buf.initialized().len() - c >= remaining {
-            match block_on(rocket::tokio::io::AsyncReadExt::read_exact(
+            try_poll!(rocket::tokio::io::AsyncReadExt::read_exact(
                 &mut self.stream,
                 &mut buf.initialized_mut()[c..remaining + c],
-            )) {
-                Ok(_) => (),
-                Err(e) => return Poll::Ready(Err(e)),
-            };
+            )
+            .boxed()
+            .poll_unpin(cx));
             self.idx = self.idx + 1;
             self.wrote_boundary_header = false;
             return Poll::Ready(Ok(()));
         }
 
         // Read the next chunk of the range
-        match block_on(rocket::tokio::io::AsyncReadExt::read(
+        try_poll!(rocket::tokio::io::AsyncReadExt::read(
             &mut self.stream,
             &mut buf.initialized_mut()[c..],
-        )) {
-            Ok(_) => Poll::Ready(Ok(())),
-            Err(e) => Poll::Ready(Err(e)),
-        }
+        )
+        .boxed()
+        .poll_unpin(cx));
+        Poll::Ready(Ok(()))
     }
 }
